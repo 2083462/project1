6ba036091455d223cdd132349742f4738e2dafdd
2083462 <2083462@talnet.nl>
2020-09-07 09:48:19 +0200

    test

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+    <title>Inloggen</title>
+</head>
+
+<body>
+    <form id="login">
+        <input type="text" id="username" placeholder="Username" required><br><br>
+        <input type="password" id="password" placeholder="Password" required><br><br>
+        <input type="submit" value="login"><br>
+        <a href="signup.php">registreren</a><br>
+        <a href="lostpsw.php">Wachtwoord vergeten?</a><br>
+    </form>
+
+</body>
+
+</html>
---
+++ lostpsw.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+    <title>password vergeten</title>
+</head>
+
+<body>
+
+<form id="password vergeten">
+    <input type="email" name="email" id="email" required><br><br>
+    <input type="submit" action="">
+</form>
+
+</body>
+
+</html>
