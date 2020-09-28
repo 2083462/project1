3c9936776a9d6209cd9c3c3107138b97a299aeb1
2083462 <2083462@talnet.nl>
2020-09-28 13:07:28 +0200

    Week 1 Les 2 - Forms / Week 3 - Les 1

--- signup.php
+++ signup.php
@@ -7,15 +7,15 @@
 <body>
 
     <form id="register">
-    <input type="text" name="voornaam" id="voornaam" placeholder="voornaam" required ><br><br>
-    <input type="text" name="tussenvoegsel" id="tussenvoegsel" placeholder="tussenvoegsel (optioneel)"><br><br>
-    <input type="text" name="achternaam" id="achternaam" placeholder="achternaam" required><br><br>
-    <input type="email" name="email" id="email" placeholder="email" required><br><br>
-    <input type="text" name="username" id="username" placeholder="username" required><br><br>
-    <input type="password" name="password" id="password" placeholder="password" required><br><br>
-    <input type="password" name="repassword" id="repassword" placeholder="repeat password" required><br><br>
-    <input type="submit" value="registreren"><br><br>
-    <a href="index.php">Terug naar login</a>
+        <input type="text" name="voornaam" id="voornaam" placeholder="voornaam" required ><br><br>
+        <input type="text" name="tussenvoegsel" id="tussenvoegsel" placeholder="tussenvoegsel (optioneel)"><br><br>
+        <input type="text" name="achternaam" id="achternaam" placeholder="achternaam" required><br><br>
+        <input type="email" name="email" id="email" placeholder="email" required><br><br>
+        <input type="text" name="username" id="username" placeholder="username" required><br><br>
+        <input type="password" name="password" id="password" placeholder="password" required><br><br>
+        <input type="password" name="repassword" id="repassword" placeholder="repeat password" required><br><br>
+        <input type="submit" value="registreren"><br><br>
+        <a href="index.php">Terug naar login</a>
     </form>
 
 </body>
